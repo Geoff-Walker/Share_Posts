ade78d26e8f9894918c09e36d65b2ef3588049cb
Geoff-Walker <geoffwalker1980@gmail.com>
2022-03-30 10:43:40 +0100

    Prod Commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'u418611748_geoff');
+  define('DB_PASS', 'Password123');
+  define('DB_NAME', 'u418611748_shareposts');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://share-posts.geoff-walker.io/');
+  // Site Name
+  define('SITENAME', 'SharePosts');
+  // App Version
+  define('APPVERSION', '1.0.0');
